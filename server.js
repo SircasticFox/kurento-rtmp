c76cf43111a092aacddeecec7357a377855d597a
Andreas Fuchs <Andreas.Fuchs@hpe.com>
2020-03-11 18:25:56 +0100

    updated some HLS settings, shorter chunks

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ const rtmp_server_config = {
             app: 'live',
             ac: 'aac',
             hls: true,
-            hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
+            hlsFlags: '[hls_time=1:hls_list_size=3:hls_flags=delete_segments]',
             dash: true,
             dashFlags: '[f=dash:window_size=3:extra_window_size=5]'
         }
@@ -368,7 +368,7 @@ function bindFFmpeg(streamip, streamport, sdpData, ws) {
         '-i', path.join(__dirname, streamip + '_' + streamport + '.sdp'),
         '-vcodec', 'copy',
         '-acodec', 'copy',
-	'-g', '24',
+        '-g', '24',
         '-f', 'flv',
         'rtmp://localhost:1234/live/desktop' //+ streamport
     ].concat();
